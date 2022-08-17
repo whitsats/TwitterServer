26124eeded632dab1eacc7fb669ecec72f54ee80
whitsats <496173602@qq.com>
2022-08-17 17:30:12 +0800

    add userSchema

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,15 +1,43 @@
 const postsResolvers = require('./posts')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const User = require('../../models/User')
+const config = require('../../config')
 
 module.exports = {
   Query: {
     ...postsResolvers.Query
   },
   Mutation: {
-    register(
+    async register(
       _,
       { registerInput: { username, email, password, confirmpassword } },
       context,
       info
-    ) {}
+    ) {
+      password = await bcrypt.hash(password, 12)
+      const newUser = new User({
+        email,
+        username,
+        password,
+        creatAt: new Date().toISOString()
+      })
+      const res = await newUser.save()
+      const token = jwt.sign(
+        {
+          id: res.id,
+          email: res.email,
+          username: res.username
+        },
+        config.SECRET_TOKEN,
+        { expiresIn: '1h' }
+      )
+
+      return {
+        ...res._doc,
+        id: res._id,
+        token
+      }
+    }
   }
 }
